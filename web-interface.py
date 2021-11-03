43a23a7060ce36e48bfd25145cc0f73c041e9e87
David Cian <cian_david@outlook.com>
2021-11-03 01:25:40 +0100

    Add webcam pose display

--- web-interface.py
+++ web-interface.py
@@ -1,7 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
+
+import cv2
+
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
 
 app = Flask(__name__)
 
+def gen(video_source):
+  with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+    while video_source.isOpened():
+      success, image = video_source.read()
+      if not success:
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue
+
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = pose.process(image)
+
+      # Draw the pose annotation on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      mp_drawing.draw_landmarks(
+          image,
+          results.pose_landmarks,
+          mp_pose.POSE_CONNECTIONS,
+          landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+      # Encode the image as jpeg
+      ret, jpeg = cv2.imencode('.jpg', image)
+
+      frame = jpeg.tobytes()
+
+      yield (b'--frame\r\n'
+             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
 @app.route("/")
 def hello_world():
-  return render_template('index.html', user_name="John")
+  cap = cv2.VideoCapture(0)
+
+  return Response(gen(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+  #return render_template('index.html', user_name="John")
