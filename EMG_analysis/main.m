063b43d37669c4c071c0b5a1426233f84de48a08
Loïc Comeliau <loic.comeliau@gmail.com>
2021-11-16 17:26:11 +0100

    small changes

--- EMG_analysis/main.m
+++ EMG_analysis/main.m
@@ -45,19 +45,61 @@ for i=1:7
         'Fs', mat2cell(cfg.Fs(1, EMG_idx), 1, nb_EMG));
 end
 
-%% Normalization
+% We could correct all the samples to have the same sampling frequency 
+% using for example : 
+% interp1(configs_EMG(1).Time{1,5}, configs_EMG(1).Data{1,5}, ...
+%     configs_EMG(1).Time{1,1})
 
-% Collect baseline for each EMG (and acc, gyr blabla)
+
+%% Baseline computation and correction
+
+% Plot baseline for each EMG channel (i.e. each muscle)
+% for i=1:nb_EMG
+%     subplot(8, 1, i)
+%     plot(baseline_EMG.Time{1,i}, baseline_EMG.Data{1,i})
+% end
+
+% Collect baseline for each EMG and correction
 muscle_baseline = zeros(1, length(baseline_EMG.Data));
 for i=1:nb_EMG
+    % Baseline computation
     muscle_baseline(i) = mean(baseline_EMG.Data{1,i});
+    % MVC correction
+    MVC_EMG.Data{1,i} = MVC_EMG.Data{1,i} - muscle_baseline(i);
+    % Configs correction
+    for j=1:length(configs_EMG)
+        configs_EMG(j).Data{1,i} = configs_EMG(j).Data{1,i} ...
+            - muscle_baseline(i);
+    end
 end
 
-% Plot VMC for each EMG channel (i.e. each muscle)
+
+%% VMC computation
+% Plot MVC for each EMG channel (i.e. each muscle)
 for i=1:nb_EMG
     subplot(8, 1, i)
     plot(MVC_EMG.Time{1,i}, MVC_EMG.Data{1,i})
 end
 
-% Compute max values for each muscle
+movAvg = dsp.MovingAverage(666);
+movRMS = dsp.MovingRMS(666);
+emg_avg = movAvg(abs(configs_EMG(1).Data{1,1}));
+emg_rms = movRMS(abs(configs_EMG(1).Data{1,1}));
+figure(1); plot(configs_EMG(1).Time{1,1}, abs(configs_EMG(1).Data{1,1}))
+figure(2); plot(configs_EMG(1).Time{1,1}, emg_avg)
+figure(3); plot(configs_EMG(1).Time{1,1}, emg_rms)
+figure(4); hold on
+plot(configs_EMG(1).Time{1,1}, abs(configs_EMG(1).Data{1,1}))
+plot(configs_EMG(1).Time{1,1}, emg_avg)
+plot(configs_EMG(1).Time{1,1}, emg_rms)
 
+% envelope(abs(configs_EMG(1).Data{1,1}),666,'rms') ?
+
+% Plan : 
+% - remove baseline, smooth data
+% - compute MVC (mean amplitude of the highest signal portion with
+% e.g. 500 ms duration)
+% - scale (smoothed) data from configs by VMC
+% - create epochs using the trigger channel
+% - quantify the amplitude of the signal of each contraction and build
+% stats for each configuration
