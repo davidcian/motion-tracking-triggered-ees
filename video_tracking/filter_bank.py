02362e32258881c6f9f51ac5cf0f667ddd6fe92c
David Cian <cian_david@outlook.com>
2021-12-09 13:44:44 +0100

    Refactor filters a bit

--- video_tracking/filter_bank.py
+++ video_tracking/filter_bank.py
@@ -1,4 +1,4 @@
-def hampel_filter(prev_values, current_value, window_size=10, window_offset=-5, k=1.4826, n_accepted_stds=3, replacement='median'):
+def hampel_detector(prev_values, current_value, window_size=10, window_offset=-5, k=1.4826, n_accepted_stds=3):
   #print("Prev values", prev_values)
   #print("Current value", current_value)
   all_values = prev_values + [current_value]
@@ -22,12 +22,24 @@ def hampel_filter(prev_values, current_value, window_size=10, window_offset=-5, 
 
   #print("Current value {} Median value {}".format(current_value, window_median))
 
+  return abs(current_value - window_median) > n_accepted_stds * est_std, window_median
+
+def hampel_filter(prev_values, current_value, window_size=10, window_offset=-5, k=1.4826, n_accepted_stds=3, replacement='median'):
+  is_anomaly, window_median = hampel_detector(prev_values, current_value, window_size=window_size, window_offset=window_offset, k=k, n_accepted_stds=n_accepted_stds)
+
   if replacement == 'median':
     replacement_value = window_median
   else:
     replacement_value = 0
 
-  return replacement_value if abs(current_value - window_median) > n_accepted_stds * est_std else current_value
+  return replacement_value if is_anomaly else current_value
 
 def identity_filter(current_value):
   return current_value
+
+class StatefulHampel():
+  def __init__(self):
+    self.anomaly_detected = False
+
+  def filter(prev_values, current_value, window_size=10, window_offset=-5, k=1.4826, n_accepted_stds=3, replacement='median'):
+    pass
